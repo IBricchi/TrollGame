b2c497c2e80902c4ad195312dea85b9433500959
= <=>
2020-05-08 22:44:46 +0200

    Add zoom controll

--- Assets/Scripts/Bob/CameraControl.cs
+++ Assets/Scripts/Bob/CameraControl.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+
 
 public class CameraControl : MonoBehaviour
 {
 	public Transform target, player;
 
 	public float mouse_sens = 150;
+	public float zoom_sens= 100;
 
 	public float clamp_angle_min = -40;
 	public float clamp_angle_max = 50;
+	public float clamp_zoom_min = 3;
+	public float clamp_zoom_max = 20;
 
 	float rot_x = 0.0f;
+	float pos_z = 3.0f;
 
 	// Update is called once per frame
 	void LateUpdate()
@@ -37,6 +43,11 @@ public class CameraControl : MonoBehaviour
 	}
 
 	void ZoomControl(){
+		float zoom = Input.GetAxis("Mouse ScrollWheel") * zoom_sens * Time.deltaTime;
 
+		pos_z += zoom;
+		pos_z = Mathf.Clamp(pos_z, -clamp_zoom_max, -clamp_zoom_min);
+
+		transform.localPosition = Vector3.forward*pos_z;
 	}
 }
