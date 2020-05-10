3ad0bc9a02d7290a3cbaa73825aa4bb4c1dc74f7
= <=>
2020-05-10 18:25:11 +0200

    Change terrain system to use chunks instead of one big terrain

---
+++ Assets/Scripts/Terrain/Chunk.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+	Mesh mesh;
+	MeshRenderer mr;
+	MeshFilter mf;
+	MeshCollider mc;
+
+	int c_x;
+	int c_z;
+	int r_seed;
+	float size;
+	int subs;
+	float depth;
+	float freq;
+
+	Vector3[] vertices;
+	int[] triangles;
+
+	public void setup(GameObject chunk_obj, int _r_seed, int x, int z, float _size, int _subs, float _depth, float _freq, Material mat)
+	{
+		mesh = new Mesh();
+
+		mr = chunk_obj.AddComponent<MeshRenderer>();
+		mr.material = mat;
+
+		mf = chunk_obj.AddComponent<MeshFilter>();
+		mf.mesh = mesh;
+
+		mc = chunk_obj.AddComponent<MeshCollider>();
+
+		c_x = x;
+		c_z = z;
+		r_seed = _r_seed;
+		size = _size;
+		subs = _subs;
+		depth = _depth;
+		freq = _freq;
+
+		create_shape();
+		update_mesh();
+	}
+
+	void create_shape(){
+		// verteces
+		vertices = new Vector3[(subs + 1) * (subs + 1)];
+		for(int vert = 0, z = 0; z <= subs; z++){
+			for(int x = 0; x <= subs; x++){
+				vertices[vert] = new Vector3(
+					c_x * size + x * size / (float)subs,
+					noise(c_x * size + x * size / (float)subs, c_z * size + z * size / (float)subs),
+					c_z * size + z * size / (float)subs
+				);
+				vert++;
+			}
+		}
+
+		// triangles
+		triangles = new int[subs * subs * 6];
+		for(int vert = 0, tri = 0, z = 0; z < subs; z++){
+			for(int x = 0; x < subs; x++){
+				triangles[tri] = vert;
+				triangles[tri + 1] = vert + subs + 1;
+				triangles[tri + 2] = vert + 1;
+
+				triangles[tri + 3] = vert + subs + 1;
+				triangles[tri + 4] = vert + subs + 2;
+				triangles[tri + 5] = vert + 1;
+
+				vert++;
+				tri += 6;
+			}
+			vert++;
+		}	
+	}
+
+	void update_mesh(){
+		mesh.Clear();
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+
+		// calculate normals for shading
+		mesh.RecalculateNormals();
+
+		// set mesh collider for collisions <- no shit Sherlock
+		// must be at end so added only once mesh has been created properly
+		mc.sharedMesh = mesh;
+	}
+
+	float noise(float a, float b){
+		return Mathf.PerlinNoise((a + r_seed) * freq, (b + r_seed) * freq) * depth;
+	}
+}
---
+++ Assets/Scripts/Terrain/ChunkLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChunkLoader : MonoBehaviour
+{
+	[SerializeField]
+	public List<Chunk> loaded_chunks;
+	
+	int lc_dim_size;
+	int lc_size;
+
+	public int r_seed = 1;
+	public int render_distance = 5;
+	public int c_size = 30;
+	public int c_subs = 10;
+	public float c_depth = 30f;
+	public float terrain_frequency = 0.01f;
+
+	public Material mat;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		loaded_chunks = new List<Chunk>();
+		lc_dim_size = (render_distance*2 + 1);
+		lc_size = lc_dim_size;
+
+		for(int z = -lc_dim_size/2; z <= lc_dim_size/2; z++){
+			for(int x = -lc_dim_size/2; x <= lc_dim_size/2; x++){
+				print(z);
+				print(x);
+				GameObject new_load_obj = new GameObject();
+				new_load_obj.layer = 8;
+
+				Chunk new_load = new_load_obj.AddComponent(typeof(Chunk)) as Chunk;
+				new_load.setup(new_load_obj, r_seed, x, z, c_size, c_subs, c_depth, terrain_frequency, mat);
+				
+				loaded_chunks.Add(new_load);
+			}
+		}
+	}
+}
